ab40268610586ed5021e235eadd0ccfc628cc4fc
Nick Ludlow <nickludlow82@gmail.com>
2016-03-01 18:43:30 +0000

    My first bash at making an audio player in swift using AVFoundation

--- firstAudioPlayer/ViewController.swift
+++ firstAudioPlayer/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var PausePlay: UIButton!
+    
+    //var ButtonAudioURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Opener", ofType: "aiff")!)
+    
+    var Opener:AVAudioPlayer = AVAudioPlayer()
+    
+    var Main:AVAudioPlayer = AVAudioPlayer()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //ButtonAudioPlayer = AVAudioPlayer(contentsOfURL: ButtonAudioURL)
+        
+        let ButtonAudioURL:NSURL = NSBundle.mainBundle().URLForResource("Opener", withExtension: "aiff")!
+        do { Opener = try AVAudioPlayer(contentsOfURL: ButtonAudioURL, fileTypeHint: nil) } catch _ { }
+        Opener.prepareToPlay()
+        
+        let BackgroundAudioURL:NSURL = NSBundle.mainBundle().URLForResource("Main", withExtension: "aiff")!
+        do { Main = try AVAudioPlayer(contentsOfURL: ButtonAudioURL, fileTypeHint: nil) } catch _ { }
+        Main.prepareToPlay()
+        
+        //Main.play()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func PlayAudio1(sender: AnyObject) {
+        Opener.play()
+    }
+    
+    @IBAction func Stop(sender: AnyObject) {
+        Main.stop()
+        Main.currentTime = 0
+        PausePlay.setTitle("Play", forState: UIControlState.Normal)
+    
+    }
+    
+    @IBAction func Restart(sender: AnyObject) {
+        Main.stop()
+        Main.currentTime = 0
+        Main.play()
+    }
+    
+    @IBAction func PausePlay(sender: AnyObject) {
+        
+        if (Main.playing == true){
+            Main.stop()
+            PausePlay.setTitle("Play", forState: UIControlState.Normal)
+        
+        } else {
+            
+            Main.play()
+            PausePlay.setTitle("Pause", forState: UIControlState.Normal)
+        
+        }
+        
+    }
+    
+    
+    
 
 }
 
